d03d46b27639541c75eed78c339f256838bb7a11
Padbras <cspecq1@Salle108>
2017-11-20 11:03:48 +0100

    Deuxieme tache réussie

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,17 +1,27 @@
 package org.ulco;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
-
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        String newtype = type.substring(0,1).toUpperCase() + type.substring(1);
+        String nomclasse = "org.ulco." + newtype;
+        
+        try {
+            o = (GraphicsObject) Class.forName(nomclasse).getConstructor(String.class).newInstance(str);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
         return o;
     }
